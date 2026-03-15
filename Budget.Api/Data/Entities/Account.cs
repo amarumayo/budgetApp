c0e3861b25a2f1fa3f72bb477796be7610a29215
amarumayo <amarumayo@gmail.com>
2026-03-15 11:40:39 -0600

    test commit

--- Budget.Api/Data/Entities/Account.cs
+++ Budget.Api/Data/Entities/Account.cs
@@ -9,7 +9,7 @@ public partial class Account
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public string Type { get; set; }
 
